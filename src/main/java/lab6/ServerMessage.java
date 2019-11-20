cf3a5e7f69301381d8944e04385013b160853ccb
Ivan <Ivan Filonenko>
2019-11-20 20:22:39 +0300

    Scripted auto-commit on change (2019-11-20 20:22:39) by gitwatch.sh

--- src/main/java/lab6/ServerMessage.java
+++ src/main/java/lab6/ServerMessage.java
@@ -1,4 +1,15 @@
 package lab6;
 
+import java.util.List;
+
 public class ServerMessage {
+    private List<String> serverPorts;
+
+    public ServerMessage(List<String> serverPorts){
+        this.serverPorts = serverPorts;
+    }
+
+    public List<String> getServerPorts(){
+        return this.serverPorts;
+    }
 }
