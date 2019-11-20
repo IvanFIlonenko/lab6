1d6ac43f683a59a5cb327c1322246839c3ef1c2a
Ivan <Ivan Filonenko>
2019-11-20 20:05:39 +0300

    Scripted auto-commit on change (2019-11-20 20:05:39) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -1,14 +1,17 @@
 package lab6;
 
 import akka.NotUsed;
-import akka.actor.Actor;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
+import akka.actor.Props;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.Route;
+import akka.pattern.Patterns;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import org.apache.zookeeper.CreateMode;
@@ -18,9 +21,13 @@ import org.apache.zookeeper.ZooKeeper;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Future;
 
-public class Main {
+public class Server extends AllDirectives {
+    private static int PORT;
+    private static ActorRef storageActor;
     private static ZooKeeper zoo;
     private static final String ROUTES = "routes";
     private static final String LOCALHOST = "localhost";
@@ -41,15 +48,16 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
         Scanner in = new Scanner(System.in);
-        int PORT = in.nextInt();
+        PORT = in.nextInt();
 
         createZoo(PORT);
 
         ActorSystem system = ActorSystem.create(ROUTES);
         http = Http.get(system);
+        storageActor = system.actorOf(Props.create(storageActor.class));
         final ActorMaterializer materializer = ActorMaterializer.create(system);
 
-        Main app = new Main();
+        Server app = new Server();
 
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.route().flow(system, materializer);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(
@@ -68,12 +76,26 @@ public class Main {
     }
 
     private Route route(){
-        try {
-            synchronized (this) {
-                wait();
-            }
-        } catch (InterruptedException e){
+        return concat(
+                get(
+                        () -> parameter("url", url ->
+                                parameter("count", count -> {
+                                    int countNumber = Integer.parseInt(count);
+                                    System.out.println("Request got from " + Integer.toString(PORT) + "count = " + count);
+                                    if (countNumber != 0) {
+                                        try {
+                                            Future<Object> randomPort = CompletableFuture.completedFuture(Patterns.ask(storageActor, new PortRandomizer(Integer.toString(PORT)), 5000));
+                                            return complete(fetch)
+                                        }
+                                    }
+                                }))
+                )
+        )
+    }
 
+    CompletionStage<HttpResponse> requestToServer(int port, String url, int count){
+        try{
+            return 
         }
     }
 }
