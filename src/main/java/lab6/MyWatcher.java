83deb53a32be81b60e1d0eeb4f57614b1c15b688
Ivan <Ivan Filonenko>
2019-11-22 12:53:17 +0300

    Scripted auto-commit on change (2019-11-22 12:53:17) by gitwatch.sh

--- src/main/java/lab6/MyWatcher.java
+++ src/main/java/lab6/MyWatcher.java
@@ -9,33 +9,3 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class MyWatcher implements Watcher {
-    @Override
-    public void process(WatchedEvent event) {
-        public void process(WatchedEvent event) {
-            List<String> servers = new ArrayList<>();
-            try{
-                servers = zoo.getChildren("/" + SERVERS, true);
-            } catch (InterruptedException | KeeperException e) {
-                e.printStackTrace();
-            }
-            List<String> serverPorts = new ArrayList<>();
-            for (String s: servers) {
-                byte[] port = new byte[0];
-                try{
-                    port = zoo.getData("/" + SERVERS + "/" + s, false, null);
-                } catch (InterruptedException | KeeperException e) {
-                    e.printStackTrace();
-                }
-                serverPorts.add(new String(port));
-            }
-            Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
-            try {
-                TimeUnit.SECONDS.sleep(3);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            process(event);
-        }
-    }
-}
