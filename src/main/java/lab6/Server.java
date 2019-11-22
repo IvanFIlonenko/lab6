1711b44c429c7dbab3a71f64a5b037d1a945f177
Ivan <Ivan Filonenko>
2019-11-22 12:53:59 +0300

    Scripted auto-commit on change (2019-11-22 12:53:59) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -82,7 +82,36 @@ public class Server extends AllDirectives {
         });
     }
 
-
+    public class MyWatcher implements Watcher {
+        @Override
+        public void process(WatchedEvent event) {
+            public void process(WatchedEvent event) {
+                List<String> servers = new ArrayList<>();
+                try{
+                    servers = zoo.getChildren("/" + SERVERS, true);
+                } catch (InterruptedException | KeeperException e) {
+                    e.printStackTrace();
+                }
+                List<String> serverPorts = new ArrayList<>();
+                for (String s: servers) {
+                    byte[] port = new byte[0];
+                    try{
+                        port = zoo.getData("/" + SERVERS + "/" + s, false, null);
+                    } catch (InterruptedException | KeeperException e) {
+                        e.printStackTrace();
+                    }
+                    serverPorts.add(new String(port));
+                }
+                Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                process(event);
+            }
+        }
+    }
 
     public static void main(String[] args) throws InterruptedException, IOException, KeeperException {
         Scanner in = new Scanner(System.in);
