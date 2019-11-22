4a6c8b82b569ae64ddd6fde6b96ff8ec6b5ef197
Ivan <Ivan Filonenko>
2019-11-22 12:49:35 +0300

    Scripted auto-commit on change (2019-11-22 12:49:35) by gitwatch.sh

--- src/main/java/lab6/Server.java
+++ src/main/java/lab6/Server.java
@@ -46,30 +46,28 @@ public class Server extends AllDirectives {
                 new Watcher() {
                     @Override
                     public void process(WatchedEvent event) {
-                        if (event.getType() == Event.EventType.NodeChildrenChanged) {
-                            List<String> servers = new ArrayList<>();
-                            try {
-                                servers = zoo.getChildren("/" + SERVERS, true);
+                        List<String> servers = new ArrayList<>();
+                        try{
+                            servers = zoo.getChildren("/" + SERVERS, true);
+                        } catch (InterruptedException | KeeperException  e) {
+                            e.printStackTrace();
+                        }
+                        List<String> serverPorts = new ArrayList<>();
+                        for (String s: servers) {
+                            byte[] port = new byte[0];
+                            try{
+                                port = zoo.getData("/" + SERVERS + "/" + s, false, null);
                             } catch (InterruptedException | KeeperException e) {
                                 e.printStackTrace();
                             }
-                            List<String> serverPorts = new ArrayList<>();
-                            for (String s : servers) {
-                                byte[] port = new byte[0];
-                                try {
-                                    port = zoo.getData("/" + SERVERS + "/" + s, false, null);
-                                } catch (InterruptedException | KeeperException e) {
-                                    e.printStackTrace();
-                                }
-                                serverPorts.add(new String(port));
-                            }
-                            Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
+                            serverPorts.add(new String(port));
                         }
-//                        try {
-//                            TimeUnit.SECONDS.sleep(3);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
+                        Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
+                        try {
+                            TimeUnit.SECONDS.sleep(3);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                         process(event);
                     }
                 }
@@ -82,30 +80,28 @@ public class Server extends AllDirectives {
         zoo.getChildren("/" + SERVERS, new Watcher() {
             @Override
             public void process(WatchedEvent event) {
-                 if (event.getType() == Event.EventType.NodeChildrenChanged) {
-                     List<String> servers = new ArrayList<>();
-                     try {
-                         servers = zoo.getChildren("/" + SERVERS, true);
-                     } catch (InterruptedException | KeeperException e) {
-                         e.printStackTrace();
-                     }
-                     List<String> serverPorts = new ArrayList<>();
-                     for (String s : servers) {
-                         byte[] port = new byte[0];
-                         try {
-                             port = zoo.getData("/" + SERVERS + "/" + s, false, null);
-                         } catch (InterruptedException | KeeperException e) {
-                             e.printStackTrace();
-                         }
-                         serverPorts.add(new String(port));
-                     }
-                     Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
-                 }
-//                try {
-//                    TimeUnit.SECONDS.sleep(3);
-//                } catch (InterruptedException e) {
-//                    e.printStackTrace();
-//                }
+                    List<String> servers = new ArrayList<>();
+                    try{
+                        servers = zoo.getChildren("/" + SERVERS, true);
+                    } catch (InterruptedException | KeeperException  e) {
+                        e.printStackTrace();
+                    }
+                    List<String> serverPorts = new ArrayList<>();
+                    for (String s: servers) {
+                        byte[] port = new byte[0];
+                        try{
+                            port = zoo.getData("/" + SERVERS + "/" + s, false, null);
+                        } catch (InterruptedException | KeeperException e) {
+                            e.printStackTrace();
+                        }
+                        serverPorts.add(new String(port));
+                    }
+                    Storage.tell(new ServerMessage(serverPorts), ActorRef.noSender());
+                try {
+                    TimeUnit.SECONDS.sleep(3);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
                 process(event);
             }
         });
